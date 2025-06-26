710030d68c77faef7a5af86a56f11ccf295a128d
cata <vlxrecata@proton.me>
2025-06-26 16:26:33 +0300

    awdaw

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,6 @@ import type { Config } from "tailwindcss"
 const { fontFamily } = require("tailwindcss/defaultTheme")
 
 const config: Config = {
-  darkMode: ["class"],
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
